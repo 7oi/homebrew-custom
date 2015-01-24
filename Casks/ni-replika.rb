553eb40b6a0104d4f8cdc84f6ba495aab8fc565e
Jói Fr. <sevenoi@sevenoi.com>
2015-01-24 19:56:41 +0000

    Fix folder error

--- Casks/ni-replika.rb
+++ Casks/ni-replika.rb
@@ -2,7 +2,7 @@ cask :v1 => 'ni-replika' do
   version '1.2.0'
   sha256 :no_check
 
-  url 'file:///Volumes/Komplete%209%20Ultimate/Others/Replika_120_Mac.dmg'
+  url 'file:///Volumes/Komplete%209%20Ultimate/Others/NI/Replika_120_Mac.dmg'
   name 'Replika'
   homepage 'http://www.native-instruments.com/en/products/komplete/effects/replika/'
   license :commercial
