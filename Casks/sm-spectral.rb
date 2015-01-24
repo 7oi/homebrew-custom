4065d35e4d5b6da100a7b5c2c54091cf94491c49
Jói Fr. <sevenoi@sevenoi.com>
2015-01-24 19:59:34 +0000

    Fix sha245 sum

--- Casks/sm-spectral.rb
+++ Casks/sm-spectral.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sm-spectral' do
   version '0.8'
-  sha256 '9aa316db1def5878aa3b0ef8a626a7940def530d86a896d8a56d0d5d0722e5d'
+  sha256 '19aa316db1def5878aa3b0ef8a626a7940def530d86a896d8a56d0d5d0722e5d'
 
   url 'http://www.michaelnorris.info/downloads/soundmagicspectralb8.dmg'
   name 'SoundMagic Spectral'
